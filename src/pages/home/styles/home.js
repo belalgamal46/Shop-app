676d2940a7244f6d31d487ccd467fe95b2e35549
belalgamal46 <belalgamal00@gmail.com>
2023-01-06 22:08:08 +0200

    Add styles to home page

---
+++ src/pages/home/styles/home.js
@@ -0,0 +1,21 @@
+import styled from 'styled-components/macro';
+
+export const Main = styled.main`
+  padding: 10rem;
+  display: flex;
+  flex-direction: column;
+  row-gap: 7rem;
+
+  @media (max-width: 600px) {
+    padding: ${({ isActive }) => (isActive ? '0' : '2rem')};
+  }
+`;
+
+export const Title = styled.h2`
+  font-size: 5rem;
+  color: #fff;
+
+  @media (max-width: 999px) {
+    display: ${({ isActive }) => (isActive ? 'none' : 'initial')};
+  }
+`;
