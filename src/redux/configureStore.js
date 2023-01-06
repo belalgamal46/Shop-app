ee6b9c2545d485ad56451ad62f69e20c4167d8a1
belalgamal46 <belalgamal00@gmail.com>
2023-01-06 08:02:35 +0200

    Create homeSlice

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,7 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
+import homeReducer from './homeSlice';
 
 const store = configureStore({
-  reducer: {}
+  reducer: {
+    homeReducer,
+  }
 });
 
 export default store;
